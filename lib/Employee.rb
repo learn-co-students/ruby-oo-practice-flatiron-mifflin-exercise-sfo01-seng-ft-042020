a465968229be7ed1a900e4916c0abf09b1b0487e
github username <github email address>
2020-04-28 06:30:40 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,66 @@
 class Employee
+    attr_accessor :name, :manager
+    attr_reader :salary
+    @@all = []
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        self.all.select { |s| s.salary > num}
+    end
+
+    def self.find_by_department(str)
+        self.all.find do |s|
+            managers_name = s.manager
+            Manager.all.select do |t|
+                managers_name.department == str
+                return s
+            end
+        end
+    end
+
+    def tax_bracket
+        arr = []
+        @@all.select do |s|
+            if s.salary > self.salary + 1000 || s.salary < self.salary - 1000
+                arr << s
+            end
+        end
+        arr
+    end
 
 end
+# **`Employee`**
+
+#   * `Employee#name`
+#     * returns a `String` that is the employee's name
+
+#   * `Employee#salary`
+#     * returns a `Fixnum` that is the employee's salary
+
+#   * `Employee#manager_name`
+#     * returns a `String` that is the name of their manager
+
+#   * `Employee.all`
+#     * returns an `Array` of all the employees
+
+#   * `Employee.paid_over`
+#     * takes a `Fixnum` argument and returns an `Array` of all the employees whose
+#      salaries are over that amount
+
+#   * `Employee.find_by_department`
+#     * takes a `String` argument that is the name of a department and returns the
+#      first employee whose manager is working in that department
+
+#   * `Employee#tax_bracket`
+#     * returns an `Array` of all the employees whose salaries are within $1000 (± 1000)
+#      of the employee who invoked the method
+
+###############################################################
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,74 @@
 class Manager
+    attr_reader :name, :department, :age
+    @@ages = []
+    @@all = []
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+        # @count += 1
+        @@ages << age
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select { |s| s.manager == self }
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+    
+    def self.all_departments
+        arr = @@all.select { |s| s }
+        array = []
+        i = 0
+        while i < arr.length do 
+            num = arr[i]
+            array << num.department
+            i += 1
+        end
+        array
+    end
+
+    def self.average_age
+        num = @@ages.sum / @@ages.length
+        return num.to_f
+    end
+
 
 end
+
+#####################################
+#   * `Manager.average_age`
+#     * returns a `Float` that is the average age of all the managers
+#   * `Manager.all_departments`
+#     * returns an `Array` of all the department names that every manager oversees
+
+#   * `Manager#hire_employee`
+#     * takes a `String` argument and a `Fixnum` argument of an employee's name and
+#      the employee's salary, respectively, and adds that employee to the list of employees that the manager oversees
+
+#   * `Manager#name`
+#     * returns a `String` that is the manager's name
+
+#   * `Manager#department`
+#     * returns a `String` that is the department that the manager oversees
+
+#   * `Manager#age`
+#     * returns a `Fixnum` that is the age of the manager
+
+#   * `Manager.all`
+#     * returns an `Array` of all the managers
+
+#   * `Manager#employees`
+#     * returns an `Array` of all the employees that the manager oversees
+
+
+
+
+
